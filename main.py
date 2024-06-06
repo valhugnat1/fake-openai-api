6dfb0ed2375a168bb049f6df74ca3adb85a6e25d
Hugo Philipp <hugo.philipp99@gmail.com>
2024-06-06 11:35:50 +0200

    Adapt to craft ai endoint

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from typing import List, Optional
-
 from pydantic import BaseModel
-
 import time
-
 from fastapi import FastAPI, Request
-
 from starlette.middleware.base import BaseHTTPMiddleware
-
+import requests
 
 class ChatMessage(BaseModel):
     role: str
@@ -41,9 +37,34 @@ async def chat_completions(request: ChatCompletionRequest):
     print(request)
     
     if request.messages and request.messages[0].role == 'user':
-      resp_content = request.messages[0].content+" As a mock AI Assitant, I can only echo your last message:" #+ request.messages[-1].content
+    #   resp_content = request.messages[0].content+" As a mock AI Assitant, I can only echo your last message:" #+ request.messages[-1].content
+
+
+        ENDPOINT_TOKEN = "XBQuxHax0-fWWPbF8HGvRzgPLMnKjDIZSYQ0Os1IU7EMBH26hRXnye6-X9a0oq7mgf18dKNgjJFVqLHJsL4K_w"
+
+        response = requests.post(
+            "https://clean-tarvos-burgundy-lambda-30.integration.craft.ai/endpoints/product-llm-emulator-depl",
+            # Endpoint inputs: Commented inputs are optional, uncomment them to use them.
+            json={
+                "frequency_penalty": 0,
+                "presence_penalty": 0,
+                "user": request.messages[0].role,
+                "messages": {"text":request.messages[0].content},
+                "stream": request.stream,
+                "top_p": 0,
+                "model": request.model,
+                "temperature": request.temperature,
+            },
+            headers={"Authorization": f"EndpointToken {ENDPOINT_TOKEN}"},
+        )
+
+        if not response.ok:
+            raise Exception(response.status_code, response.text)
+        
+        resp_content = response.json()["outputs"]["choices"][0]["message"]["content"]
+
     else:
-      resp_content = "As a mock AI Assitant, I can only echo your last message, but there were no messages!"
+        resp_content = "As a mock AI Assitant, I can only echo your last message, but there were no messages!"
 
     return {
         "id": "1337",
